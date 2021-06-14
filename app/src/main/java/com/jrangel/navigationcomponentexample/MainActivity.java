4aae0c72e26448cf4841d142d3f6b60d8087453d
Josue <josrangel@hotmail.com>
2021-06-13 23:26:24 -0500

    init commit

---
+++ app/src/main/java/com/jrangel/navigationcomponentexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jrangel.navigationcomponentexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Tutorial
+ * https://developer.android.com/guide/navigation/navigation-getting-started
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
